a4e0e2c2e256caa5f6700dee0029cf05b8f8af55
mr-hollowman <92283129+Mr-hollowman@users.noreply.github.com>
2023-08-23 12:29:48 +0530

    protected routes added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { useSelector } from 'react-redux';
 import { Box, CssBaseline, ThemeProvider, createTheme } from '@mui/material';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Login from './components/Login';
+import ProtectedRoute from './utils/ProtectedRoutes';
+import Dashboard from './components/Dashboard';
 
 function App() {
   const { theme } = useSelector(state => state.theme)
@@ -16,6 +18,7 @@ function App() {
       <CssBaseline />
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
           <Route path="/login" element={<Login />} />
         </Routes>
       </BrowserRouter>
